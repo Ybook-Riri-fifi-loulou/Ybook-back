e1b074fc5e77a5dd30e4dd9296656d2dd0a45af2
Arnal Théo <61836279+TheoArnal@users.noreply.github.com>
2023-01-05 12:25:39 +0100

    feat: create user

---
+++ src/Common/idToken.middleware.ts
@@ -0,0 +1,20 @@
+import {NextFunction, Request, Response} from "express";
+import { CognitoJwtVerifier } from "aws-jwt-verify";
+
+
+export const idTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.body.token;
+    const verifier = CognitoJwtVerifier.create({
+        region: "ew-west-3",
+        userPoolId: "id",
+        clientId: "7llslfb09dtag2h7117og3ku72",
+    });
+
+    try {
+        const payload = await verifier.verify(token, { tokenUse: "id" });
+        console.log(payload);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
