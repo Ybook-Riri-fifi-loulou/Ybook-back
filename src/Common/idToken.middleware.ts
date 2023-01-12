b3956e55c0781fd0ae76c3ae25fc12ac97770226
Arnal Théo <61836279+TheoArnal@users.noreply.github.com>
2023-01-12 11:36:24 +0100

    feat: add middleware

--- src/Common/idToken.middleware.ts
+++ src/Common/idToken.middleware.ts
@@ -1,24 +1,35 @@
 import {NextFunction, Request, Response} from "express";
 import { CognitoJwtVerifier } from "aws-jwt-verify";
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient()
 
 
 export const idTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.body.token;
     const verifier = CognitoJwtVerifier.create({
-        region: "ew-west-3",
-        userPoolId: "id",
+        tokenUse: "id",
+        userPoolId: "eu-west-3_Iekd8jDeb",
         clientId: "7llslfb09dtag2h7117og3ku72",
     });
 
     try {
-        const payload = await verifier.verify(token, { tokenUse: "id" });
-        console.log(payload);
-        if (payload) {
-            next()
+        const payload = await verifier.verify(token, {tokenUse: "id"});
+
+        res.locals.firstname = payload['given_name'];
+        res.locals.email = payload['email'];
+        res.locals.lastname = payload['name'];
+        const userAlreadyExists = await prisma.user.findFirst({
+            where: {
+                email: res.locals.email
+            }
+        })
+        if (payload && !userAlreadyExists) {
+            next();
         } else {
+            console.log("User already exists");
             next({status: 401, message: "Unauthorized"});
         }
     } catch (err) {
-        console.log(err);
+        next({status: 401, message: "Token Expired"});
     }
 }
