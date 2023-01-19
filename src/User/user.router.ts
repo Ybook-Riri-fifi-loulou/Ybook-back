041fa8b5733965c2651ee5ca46095b89ef77298e
Lucas Dindault <lucas@creabilis.com>
2023-01-19 10:58:21 +0100

    chnge body to request

--- src/User/user.router.ts
+++ src/User/user.router.ts
@@ -6,7 +6,7 @@ import {idTokenMiddleware} from "../Common/idToken.middleware";
 export const userRouter = Router();
 
 userRouter.get('/', userController.getAllUsers);
-userRouter.get('/email', userController.getUserByEmail)
+userRouter.get('/:email', userController.getUserByEmail)
 userRouter.get('/:id', userController.getUserById);
 userRouter.get('/:id', userController.getUserPosts);
 userRouter.post('/', idTokenMiddleware, userController.createUser);
