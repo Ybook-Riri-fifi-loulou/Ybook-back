cb2a02b7e4a1c2a3df723f903428d6993505421a
Lucas Dindault <lucas@creabilis.com>
2023-01-28 12:38:35 +0100

    add pending section fonctionnalities

--- src/Friendship/friendship.router.ts
+++ src/Friendship/friendship.router.ts
@@ -4,10 +4,10 @@ import {friendshipController} from "./friendship.controller";
 
 export const friendshipRouter = Router();
 
-friendshipRouter.get('/', friendshipController.getFriendships);
+friendshipRouter.get('/:id/pendingfriendship', friendshipController.getFriendships);
 friendshipRouter.get('/:id/friends', friendshipController.getFriends);
 friendshipRouter.put('/:id', friendshipController.acceptFriendship);
-friendshipRouter.put('/:id', friendshipController.declineFriendship);
+friendshipRouter.put('/:id/refused', friendshipController.declineFriendship);
 friendshipRouter.delete('/:id', friendshipController.deleteFriendship);
 
 
