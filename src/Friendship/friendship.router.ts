db500231edf633c9e275a58c50606546e3a47a6d
Lucas Dindault <lucas@creabilis.com>
2023-01-28 14:33:52 +0100

    WIP friendship

--- src/Friendship/friendship.router.ts
+++ src/Friendship/friendship.router.ts
@@ -6,6 +6,7 @@ export const friendshipRouter = Router();
 
 friendshipRouter.get('/:id/pendingfriendship', friendshipController.getFriendships);
 friendshipRouter.get('/:id/friends', friendshipController.getFriends);
+friendshipRouter.post('/:userFromEmail/:userToEmail', friendshipController.addFrienship);
 friendshipRouter.put('/:id', friendshipController.acceptFriendship);
 friendshipRouter.put('/:id/refused', friendshipController.declineFriendship);
 friendshipRouter.delete('/:id', friendshipController.deleteFriendship);
