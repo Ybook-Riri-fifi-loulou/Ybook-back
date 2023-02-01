cde3c720e65cfde5cdcc738b192d64ebe12cf25d
Arnal Théo <61836279+TheoArnal@users.noreply.github.com>
2023-02-01 19:14:23 +0100

    feat: ajout gestion image avec s3

--- src/Common/s3.service.ts
+++ src/Common/s3.service.ts
@@ -1,8 +1,9 @@
 import AWS from "aws-sdk";
 
 class S3Service {
+    //Pre-signing a putObject operation with a specific payload
      async getSignedUrl() {
-        const key = "image/rifilou"
+        const key = "rifilou"
         const params = {Bucket: 'ybook-dev', Key: key, Expires: 60 , ContentType: "image/jpeg"}
 
         const s3 = new AWS.S3({
@@ -14,6 +15,21 @@ class S3Service {
             return url
         });
         return {url, key}
+    }
+
+    //Pre-signing a getObject operation
+    async getSignedUrlGet(key: string) {
+        const params = {Bucket: 'ybook-dev', Key: key, Expires: 60}
+
+        const s3 = new AWS.S3({
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            region: "eu-west-3"
+        });
+        const url = await s3.getSignedUrlPromise('getObject', params).then((url) => {
+            return url
+        });
+        return url
     }
 }
 
